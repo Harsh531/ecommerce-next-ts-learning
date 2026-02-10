9db8f189b2cf395632a167d01f047db7aac1d8e2
Harsh531 <62638966+Harsh531@users.noreply.github.com>
2026-02-10 07:01:48 +0000

    Update package.json to use concurrently for dev and start scripts; add json-server config file

--- src/lib/features/api/apiSlice.ts
+++ src/lib/features/api/apiSlice.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: process.env.NEXT_PUBLIC_API_URL || "/api",
+        baseUrl: process.env.NEXT_PUBLIC_API_URL || "/",
     }),
     tagTypes: ['Product'],
     endpoints: (builder) => ({
