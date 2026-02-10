9c89af471876b3e0824d81f1b76cea2bc9ad9e6c
Harsh531 <62638966+Harsh531@users.noreply.github.com>
2026-02-10 06:10:37 +0000

    Update base URL in apiSlice to use relative path

--- src/lib/features/api/apiSlice.ts
+++ src/lib/features/api/apiSlice.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react"
 export const apiSlice = createApi({
     reducerPath: "api",
     baseQuery: fetchBaseQuery({
-        baseUrl: "http://localhost:4000",
+        baseUrl: "/api",
     }),
     tagTypes: ['Product'],
     endpoints: (builder) => ({
